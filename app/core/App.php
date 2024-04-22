9ee33fd54b929c6461efa57a339e514a13889c1d
eyondk <yronpadilla123@gmail.com>
2024-04-22 23:55:46 +0800

    Login Testing

--- app/core/App.php
+++ app/core/App.php
@@ -23,10 +23,19 @@ class App{
         }
         
         else{
+            $filename = "../app/controllers/".ucfirst($URL[0])."/".ucfirst($URL[0]).".php";
+    
+            if(file_exists($filename)){
+                require $filename;
+                $this->controller = ucfirst($URL[0]);
+            }
+            
+            else{
             $filename = "../app/controllers/_404.php";
             require $filename;
             $this->controller = "_404";
         }
+    }
         
         $controller = new $this->controller;
         call_user_func_array([$controller,$this->method], []);
