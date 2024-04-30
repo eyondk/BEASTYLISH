1c7b778486c0a5e97631f65a53cc4b43ad1937fb
Christine Jade L. Duran <jadeduranchristine@gmail.com>
2024-05-01 06:01:52 +0800

    Add files via upload

---
+++ header/home.php
@@ -0,0 +1,120 @@
+<?php require('header.inc.php')?>
+ <!--new home collecction section-->
+    <section>
+      <div class="newcoll"><marquee behavior="scroll" direction="circle" scrollamount="8">NEW HOME COLLECTION • NEW HOME COLLECTION • NEW HOME COLLECTION</marquee></div>  
+      <div class="line"></div>
+    </section>
+
+<header>
+    <div class="Container">
+        <div class="row">  
+           <div class="col-md-4 col-sm-1">
+            <h1>WHERE</h1>
+            <h1>AFFORDABILITY</h1>
+            <h1>MEETS</h1>
+            <h1>QUALITY</h1>
+            <p>HELLO IS IT ME YOUR LOOKING FOR?</p>
+            <a href="#!" class="btnShop">Shop Now</a>  
+           </div>
+           <div class="col-md-8 col-sm-4 justify-content-center">
+            <div id="carouselExampleControls" class="carousel slide" data-mdb-ride="carousel" data-mdb-carousel-init>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="card-wrapper">
+                            <div class="card">
+                                <img src="img/daisy.jpg" class="card-img-top"/>
+                                <div class="card-body">
+                                <h5 class="card-title">WHITE DAISY</h5> 
+                                <p class="card-price">&#8369;100.00</p>
+                                <a href="#!" class="btn" data-mdb-ripple-init>Add to Cart</a>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="img/tshirt.jpg" class="card-img-top"/>
+                                <div class="card-body">
+                                <h5 class="card-title">DOUBLE LINEN SHIRT</h5>
+                                <p class="card-price">&#8369;100.00</p>
+                                <a href="#!" class="btn" data-mdb-ripple-init>Add to Cart</a>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="img/tshirt.jpg" class="card-img-top"/>
+                                <div class="card-body">
+                                <h5 class="card-title">DOUBLE LINEN SHIRT</h5>
+                                <p class="card-price">&#8369;100.00</p>
+                                <a href="#!" class="btn" data-mdb-ripple-init>Add to Cart</a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="card-wrapper">
+                            <div class="card">
+                                <img src="img/tshirt.jpg" class="card-img-top"/>
+                                <div class="card-body">
+                                <h5 class="card-title">DOUBLE LINEN SHIRT</h5>
+                                <p class="card-price">&#8369;100.00</p>
+                                <a href="#!" class="btn" data-mdb-ripple-init>Add to Cart</a>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="img/daisy.jpg" class="card-img-top"/>
+                                <div class="card-body">
+                                <h5 class="card-title">WE DAISY</h5> 
+                                <p class="card-price">&#8369;100.00</p>
+                                <a href="#!" class="btn" data-mdb-ripple-init>Add to Cart</a>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="img/tshirt.jpg" class="card-img-top"/>
+                                <div class="card-body">
+                                <h5 class="card-title">DOU LINEN SHIRT</h5>
+                                <p class="card-price">&#8369;100.00</p>
+                                <a href="#!" class="btn" data-mdb-ripple-init>Add to Cart</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="card-wrapper">
+                            <div class="card">
+                                <img src="img/daisy.jpg" class="card-img-top"/>
+                                <div class="card-body">
+                                    <h5 class="card-title">WHMPTE DAISY</h5> 
+                                    <p class="card-price">&#8369;100.00</p>
+                                    <a href="#!" class="btn" data-mdb-ripple-init>Add to Cart</a>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="img/tshirt.jpg" class="card-img-top"/>
+                                <div class="card-body">
+                                    <h5 class="card-title">DOE LINEN SHIRT</h5>
+                                    <p class="card-price">&#8369;100.00</p>
+                                    <a href="#!" class="btn" data-mdb-ripple-init>Add to Cart</a>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="img/tshirt.jpg" class="card-img-top"/>
+                                <div class="card-body">
+                                    <h5 class="card-title">SHEETLINEN SHIRT</h5>
+                                    <p class="card-price">&#8369;100.00</p>
+                                    <a href="#!" class="btn" data-mdb-ripple-init>Add to Cart</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-mdb-target="#carouselExampleControls" data-mdb-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-mdb-target="#carouselExampleControls" data-mdb-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</header>
+<script src="js/home.js"></script>t
