1336c0d451a1d7e3eb08ab47967e0519c69d1227
Christine Jade L. Duran <jadeduranchristine@gmail.com>
2024-05-01 05:59:13 +0800

    Add files via upload

---
+++ header/header.inc.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>Be'Astylish</title>
+    <!-- MDB icon -->
+    <link rel="icon" href="img/beastylish-favicon.png" type="image" />
+    <!-- Font Awesome -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+    />
+    <!-- Google Fonts Poppins -->
+    <link  rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap">
+    <!-- MDB -->
+    <link rel="stylesheet" href="css/mdb.min.css" />
+    <!--akoa css-->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="css/contact.css">
+  </head>
+  <body>
+    <!-- Start your project here-->
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+            <button
+                data-mdb-collapse-init
+                class="navbar-toggler"
+                type="button"
+                data-mdb-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+                >
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" href="#">HOME</a>
+                    <a class="nav-link" href="#">SHOP</a>
+                    <a class="nav-link" href="#">ABOUT</a>
+                    <a class="nav-link" id="brandname">BEASTYLISH</a>
+                    <a class="nav-link" href="#">ACCOUNT</a>
+                    <a class="nav-link" href="#">CART</a>
+                    <a class="nav-link" href="#">CONTACT</a>
+                </div>
+            </div>
+        </div>
+    </nav>
