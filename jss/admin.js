900bf7275090599a24b72a37aa2e9894bff3df37
Christine Jade L. Duran <jadeduranchristine@gmail.com>
2024-04-30 18:42:18 +0800

    Delete jss directory

--- jss/admin.js
+++
@@ -1,7 +0,0 @@
-const body = document.querySelector("body"),
-      sidebar = document.querySelector(".sidebar"),
-      toggle = document.querySelector(".toggle");
-
-      toggle.addEventListener("click", () =>{
-        sidebar.classList.toggle("close");
-      });
