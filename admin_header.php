15607e2188535ddffd2f8b0e3820ae009b1ef9cf
Christine Jade L. Duran <jadeduranchristine@gmail.com>
2024-05-02 22:01:39 +0800

    Update admin_header.php

--- admin_header.php
+++ admin_header.php
@@ -18,6 +18,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/admin.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <link rel="shortcut icon" href="img/Beastylish-favicon.png" type="image/x-icon">
     <title>Admin Page</title>
 </head>
 <body>
