3dea4d2c742a454988e96b90772a8e1a3b19ca5d
Christine Jade L. Duran <jadeduranchristine@gmail.com>
2024-05-03 16:39:16 +0800

    Update home.php

--- home.php
+++ home.php
@@ -15,11 +15,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Home | Beastylish</title>
+    <link rel="shortcut icon" href="img/Beastylish-favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/home.css">
 </head>
 <body>
 
 <?php include("home_header.inc.php");?>
-    
+<section class="home">
+    <div class="new">NEW HOME COLLECTION</div>
+    <?php include("latestprod.php")?>
+</section>    
 </body>
 </html>
