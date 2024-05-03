ec2d540d6202fec74e8d65599e94f7be8359b764
Christine Jade L. Duran <jadeduranchristine@gmail.com>
2024-05-03 16:42:32 +0800

    Add files via upload

---
+++ latestprod.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Swiper CSS -->
+    <link rel="stylesheet" href="css/swiper-bundle.min.css" />
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/latestprod.css" />
+  </head>
+  <body>
+    <div class="container swiper">
+      <a href="#" class="view-all">View all products &#10230;</a>
+      <div class="slide-container">
+        <div class="card-wrapper swiper-wrapper">
+          <div class="card swiper-slide">
+            <div class="image-box">
+              <img src="images/showImg/fullDev.jpg" alt="" />
+            </div>
+            <div class="product-details">
+              <div class="prod_name-price">
+                <h3 class="prod_name">SINBUYASSS</h3>
+                <h4 class="price">&#8369; 700.00</h4>
+              </div>
+              <div class="button-box">
+                <button class="product-button">ADD TO CART</button>
+              </div>
+            </div>
+          </div>
+          <div class="card swiper-slide">
+            <div class="image-box">
+              <img src="images/showImg/photographer.jpg" alt="" />
+            </div>
+            <div class="product-details">
+              <div class="prod_name-price">
+                <h3 class="prod_name">Priase</h3>
+                <h4 class="price">&#8369; 700.00</h4>
+              </div>
+              <div class="button-box">
+                <button class="product-button">ADD TO CART</button>
+              </div>
+            </div>
+          </div>
+          <div class="card swiper-slide">
+            <div class="image-box">
+              <img src="images/showImg/dataAna.jpg" alt="" />
+            </div>
+            <div class="product-details">
+              <div class="prod_name-price">
+                <h3 class="prod_name">TInola</h3>
+                <h4 class="price">&#8369; 700.00</h4>
+              </div>
+              <div class="button-box">
+                <button class="product-button">ADD TO CART</button>
+              </div>
+            </div>
+          </div>
+          <div class="card swiper-slide">
+            <div class="image-box">
+              <img src="images/showImg/appDev.jpg" alt="" />
+            </div>
+            <div class="product-details">
+              <div class="prod_name-price">
+                <h3 class="prod_name">HAKDOGe</h3>
+                <h4 class="price">&#8369; 700.00</h4>
+              </div>
+              <div class="button-box">
+                <button class="product-button">ADD TO CART</button>
+              </div>
+            </div>
+          </div>
+          <div class="card swiper-slide">
+            <div class="image-box">
+              <img src="images/showImg/blogger.jpg" alt="" />
+            </div>
+            <div class="product-details">
+              <div class="prod_name-price">
+                <h3 class="prod_name">Priase333</h3>
+                <h4 class="price">&#8369; 700.00</h4>
+              </div>
+              <div class="button-box">
+                <button class="product-button">ADD TO CART</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="swiper-button-next swiper-navBtn"></div>
+      <div class="swiper-button-prev swiper-navBtn"></div>
+      <div class="swiper-pagination"></div>
+    </div>
+
+    <script src="js/swiper-bundle.min.js"></script>
+    <script src="js/latestprod.js"></script>
+  </body>
+</html>
