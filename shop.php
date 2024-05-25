cc351aafc6f563cbc7e586661a7d3c1744d74eba
Christine Jade L. Duran <jadeduranchristine@gmail.com>
2024-05-26 03:15:00 +0800

    Add files via upload

---
+++ shop.php
@@ -0,0 +1,289 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop | Beastylish</title>
+    <link rel="shortcut icon" href="img/Beastylish-favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/shop.css">
+    <link rel="stylesheet" href="css/latestprod.css">
+
+</head>
+<body>
+    <?php require("user_header.inc.php")?>
+
+
+    <div class="flex">
+        <div class="shopCat">
+            <ul>
+                <li ><a href="#" class="shopAll">SHOP ALL</a></li>
+                <li ><a href="#" class="shopNew">NEW</a></li>
+                <li ><a href="#" class="shopSale">SALE</a></li>
+                <li>
+                    <a href="#" id="acc-btn" class="acc">ACCESSORIES
+                    <span class="fa fa-chevron-down first"></span>
+                    </a>
+                    <ul id="acc-show">
+                    <li><a href="#" class="first-sub">Necklace</a></li>
+                    <li><a href="#">Bracelet</a></li>
+                    <li><a href="#">Sunglass</a></li>
+                    <li><a href="#">Earring</a></li>
+                    <li><a href="#">Clip</a></li>
+                    <li><a href="#">Anklet</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#" id="brandnew-btn" class="brand">BRAND NEW
+                    <span class="fa fa-chevron-down second"></span>
+                    </a>
+                    <ul id="brandnew-show">
+                    <li><a href="#" class="first-sub">Tops</a></li>
+                    <li><a href="#">Bottoms</a></li>
+                    <li><a href="#">Dress</a></li>
+                    <li><a href="#">Swimwear</a></li>
+
+                    </ul>
+                </li>
+                <li>
+                    <a href="#" id="preloved-btn" class="pre">PRE-LOVED
+                    <span class="fa fa-chevron-down third"></span>
+                    </a>
+                    <ul id="preloved-show">
+                    <li><a href="#" class="first-sub">Tops</a></li>
+                    <li><a href="#">Bottoms</a></li>
+                    <li><a href="#">Dress</a></li>
+                    <li><a href="#">Swimwear</a></li>
+                    </ul>
+                </li>
+                <li><a href="#" class="hand">HANDMADE BOUQUET</a></li>
+            </ul>
+        </div>
+
+        <div class="product-display">
+            <div class="searchbar">
+                <button type="submit" id="search-btn" class="search"><i class="fa-solid fa-magnifying-glass"></i></button>
+                <input type="search" name="" id="" class="searchInput" placeholder="Search for Products">
+            </div>
+
+            <div class="product-gallery">
+                <div class="product-card">
+                    <div class="sale-details">
+                        <p class="sale">5&#37;</p>
+                    </div>
+                    <div class="main-images">
+                        <img class="product-img" src="img/tshirt.jpg">
+                    </div>
+                    <div class="details">
+                        <span class="product-name">ADDIDAS GAZE</span>
+                    </div>
+                    <div class="size-price">
+                        <div class="price">
+                        <span class="price-num">&#x20B1;9.00</span>
+                        <span class="price-num"><del>&#x20B1;12.00</del></span>
+                        </div>
+                    </div>
+                    <div class="button">
+                        <button type="button" class="buy-btn">Buy Now</button>
+                        <button type="button" class="add-btn">Add To Cart</button>
+                    </div>
+                </div>
+                
+
+                <div class="product-card">
+                    <div class="sale-details">
+                        <p class="sale">5&#37;</p>
+                    </div>
+                    <div class="main-images">
+                        <img class="product-img" src="img/tshirt.jpg">
+                    </div>
+                    <div class="details">
+                        <span class="product-name">ADDIDAS GAZE</span>
+                    </div>
+                    <div class="size-price">
+                        <div class="price">
+                        <span class="price-num">&#x20B1;9.00</span>
+                        <span class="price-num"><del>&#x20B1;12.00</del></span>
+                        </div>
+                    </div>
+                    <div class="button">
+                        <button type="button" class="buy-btn">Buy Now</button>
+                        <button type="button" class="add-btn">Add To Cart</button>
+                    </div>
+                </div>
+
+                <div class="product-card">
+                    <div class="sale-details">
+                        <p class="sale">5&#37;</p>
+                    </div>
+                    <div class="main-images">
+                        <img class="product-img" src="img/tshirt.jpg">
+                    </div>
+                    <div class="details">
+                        <span class="product-name">ADDIDAS GAZE</span>
+                    </div>
+                    <div class="size-price">
+                        <div class="price">
+                        <span class="price-num">&#x20B1;9.00</span>
+                        <span class="price-num"><del>&#x20B1;12.00</del></span>
+                        </div>
+                    </div>
+                    <div class="button">
+                        <button type="button" class="buy-btn">Buy Now</button>
+                        <button type="button" class="add-btn">Add To Cart</button>
+                    </div>
+                </div>
+
+                <div class="product-card">
+                    <div class="sale-details">
+                        <p class="sale">5&#37;</p>
+                    </div>
+                    <div class="main-images">
+                        <img class="product-img" src="img/tshirt.jpg">
+                    </div>
+                    <div class="details">
+                        <span class="product-name">ADDIDAS GAZE</span>
+                    </div>
+                    <div class="size-price">
+                        <div class="price">
+                        <span class="price-num">&#x20B1;9.00</span>
+                        <span class="price-num"><del>&#x20B1;12.00</del></span>
+                        </div>
+                    </div>
+                    <div class="button">
+                        <button type="button" class="buy-btn">Buy Now</button>
+                        <button type="button" class="add-btn">Add To Cart</button>
+                    </div>
+                </div>
+
+                <div class="product-card">
+                    <div class="sale-details">
+                        <p class="sale">5&#37;</p>
+                    </div>
+                    <div class="main-images">
+                        <img class="product-img" src="img/tshirt.jpg">
+                    </div>
+                    <div class="details">
+                        <span class="product-name">ADDIDAS GAZE</span>
+                    </div>
+                    <div class="size-price">
+                        <div class="price">
+                        <span class="price-num">&#x20B1;9.00</span>
+                        <span class="price-num"><del>&#x20B1;12.00</del></span>
+                        </div>
+                    </div>
+                    <div class="button">
+                        <button type="button" class="buy-btn">Buy Now</button>
+                        <button type="button" class="add-btn">Add To Cart</button>
+                    </div>
+                </div>
+
+                <div class="product-card">
+                    <div class="sale-details">
+                        <p class="sale">5&#37;</p>
+                    </div>
+                    <div class="main-images">
+                        <img class="product-img" src="img/tshirt.jpg">
+                    </div>
+                    <div class="details">
+                        <span class="product-name">ADDIDAS GAZE</span>
+                    </div>
+                    <div class="size-price">
+                        <div class="price">
+                        <span class="price-num">&#x20B1;9.00</span>
+                        <span class="price-num"><del>&#x20B1;12.00</del></span>
+                        </div>
+                    </div>
+                    <div class="button">
+                        <button type="button" class="buy-btn">Buy Now</button>
+                        <button type="button" class="add-btn">Add To Cart</button>
+                    </div>
+                </div>
+            </div>
+
+            <!-- The Modal -->
+            <div id="myModal" class="modal">
+                <!-- Modal content -->
+                <div class="modal-content">
+                    <span class="close">&times;</span>
+                    <div class="product-card">
+                        <div class="sale-details">
+                            <p class="sale">5&#37;</p>
+                        </div>
+                        <div class="main-images">
+                            <img class="product-img" src="img/tshirt.jpg">
+                        </div>
+                        <div class="details">
+                            <span class="product-name">ADDIDAS GAZE</span>
+                            <p class="prod-det">Lorem ipsum dolor sit lorenm i amet, consectetur adipisicing elit. Eum, ea, ducimus!</p>
+
+                        </div>
+                        <div class="size-price">
+                            <div class="price">
+                                <span class="price-num">&#x20B1;9.00</span>
+                                <span class="origprice-num"><del>&#x20B1;12.00</del></span>
+                            </div>
+                            <div class="content size">
+                                <div class="name size-name">
+                                    Size:
+                                </div>
+                                <div class="size-value">
+                                    <span class="color">XS</span>
+                                    <span class="color">S</span>
+                                    <span class="active">M</span>
+                                    <span class="color">L</span>
+                                    <span class="color">XL</span>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div class="button">
+                            <button type="button" class="buy-btn">Buy Now</button>
+                            <button type="button" class="add-btn">Add To Cart</button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            
+            
+
+        </div>
+    </div>
+
+
+    <script src="js/shop.js"></script>
+    <script src="https://kit.fontawesome.com/f8e1a90484.js" crossorigin="anonymous"></script>
+    <script>
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get all product cards
+    var cards = document.querySelectorAll(".product-gallery .product-card");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // Add click event listener to each product card
+    cards.forEach(function(card) {
+        card.addEventListener("click", function() {
+            modal.style.display = "block";
+        });
+    });
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+    </script>   
+
+</body>
+</html>
