dd2b33cfcbef7888272d27eaab1004a12866eee6
Christine Jade L. Duran <jadeduranchristine@gmail.com>
2024-06-08 12:47:13 +0800

    Update checkout.js

--- js/checkout.js
+++ js/checkout.js
@@ -1,10 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     var cancelbtn = document.getElementById("cancel");
+    var span = document.getElementsByClassName("closecheck");
+    var checkoutbtn = document.getElementById("checkoutbtn");
+    var checkedoutmodal = document.getElementById("checkedModal");
+    
 
-
+    
 
     cancelbtn.onclick = function (){
         window.location = "cart.php";
+    };
+
+
+    checkoutbtn.onclick = function () {
+        checkedoutmodal.style.display = "block";
+    };
+
+    
+    span.onclick = function () {
+        checkedoutmodal.style.display = "none";
+        
+    };
+
+
+    // Close modals on window click
+    window.onclick = function (event) {
+        if (event.target == checkedoutmodal) {
+            checkedoutmodal.style.display = "none";
+        }
+       
     };
 });
