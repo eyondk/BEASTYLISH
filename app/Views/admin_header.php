6d149818798e483be8d6aa49543e2943bffa0c70
Christine Jade L. Duran <jadeduranchristine@gmail.com>
2024-06-11 07:29:28 +0800

    ako gi lahi ang id and classes sa dropdownlist og btn niya

--- app/Views/admin_header.php
+++ app/Views/admin_header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?=ASSETS?>css/admin.css">
+    <link rel="stylesheet" href="<?= ASSETS ?>css/admin.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <link rel="shortcut icon" href="img/Beastylish-favicon.png" type="image/x-icon">
     <title>Admin Page</title>
@@ -28,36 +28,34 @@
             <div class="menu">
                 <ul class="menu-links">
                     <li class="nav-link">
-                        <a href="<?=ROOT?>home">
+                        <a href="<?= ROOT ?>home">
                             <i class="fa fa-dashboard icon"></i>
                             <span class="text nav-text">DASHBOARD</span>
                         </a>
                     </li>
                     <li class="nav-link">
-                        <a href="#" id="acc-btn" class="acc">
+                        <a href="#" id="prod-btn" class="products">
                             <i class="fa fa-dashboard icon"></i>
                             PRODUCTS
                             <span class="fa fa-chevron-down"></span>
                         </a>
-                        <ul id="acc-show">
-                            <li><a href="<?=ROOT?>Products" class="first-sub">Product List</a></li>
-                            <li><a href="<?=ROOT?>Category">Category</a></li>
-                            
+                        <ul id="prod-show">
+                            <li><a href="<?= ROOT ?>Products" >Product List</a></li>
+                            <li><a href="<?= ROOT ?>Category">Category</a></li>
                         </ul>
                     </li>
                     <li class="nav-link">
-                        <a href="#" id="acc-btn" class="acc">
+                        <a href="#" id="order-btn" class="order">
                             <i class='fas fa-clipboard-list icon'></i>
                             ORDERS
                             <span class="fa fa-chevron-down"></span>
                         </a>
-                        <ul id="acc-show">
-                            <li><a href="<?=ROOT?>Orders" class="first-sub">Order List</a></li>
-                            <li><a href="<?=ROOT?>OrderPending">Order Pending</a></li>
-                            <li><a href="<?=ROOT?>OrderOnDelivery">Order on Delivery</a></li>
-                            <li><a href="<?=ROOT?>OrderCompleted">Order Completed</a></li>
-                            <li><a href="<?=ROOT?>OrderCancel">Order Cancelled</a></li>
-                            
+                        <ul id="order-show" class="suborder-list">
+                            <li><a href="<?= ROOT ?>Orders">Order List</a></li>
+                            <li><a href="<?= ROOT ?>OrderPending">Order Pending</a></li>
+                            <li><a href="<?= ROOT ?>OrderOnDelivery">Order on Delivery</a></li>
+                            <li><a href="<?= ROOT ?>OrderCompleted">Order Completed</a></li>
+                            <li><a href="<?= ROOT ?>OrderCancel">Order Cancelled</a></li>
                         </ul>
                     </li>
                     <li class="nav-link">
@@ -86,35 +84,6 @@
         </div>
     </nav>
 
-    <script src="js/admin.js"></script>
-    <script>
-        document.addEventListener("DOMContentLoaded", function() {
-            const accBtn = document.getElementById("acc-btn");
-            const accShow = document.getElementById("acc-show");
-
-            if (accBtn) {
-                accBtn.addEventListener("click", function(event) {
-                    event.preventDefault();
-                    accShow.classList.toggle("show");
-                    accBtn.querySelector(".fa-chevron-down").classList.toggle("flip");
-
-                    // Adjust the position of subsequent list items
-                    const parentLi = accBtn.parentElement;
-                    const allLis = Array.from(document.querySelectorAll('.sidebar > .menu-bar > .menu-links > li'));
-                    const submenuHeight = accShow.classList.contains('show') ? accShow.scrollHeight : 0;
-
-                    let nextElement = parentLi.nextElementSibling;
-                    if (nextElement) {
-                        nextElement.style.marginTop = accShow.classList.contains('show') ? `${submenuHeight}px` : '0';
-                        nextElement = nextElement.nextElementSibling;
-                    }
-                });
-            }
-
-            
-
-            
-        });
-    </script>
+    <script src="<?= ROOT ?>/assets/js/admin.js"></script>
 </body>
 </html>
