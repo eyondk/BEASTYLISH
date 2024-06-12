fe42116c75cef7f6794a27eeaa114a16817ae06a
Christine Jade L. Duran <jadeduranchristine@gmail.com>
2024-06-12 23:36:42 +0800

    jj

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,24 +1,29 @@
 <?php
 
-/**
- *  home class
- */
-
 class Home extends Controller
-
-{  
-    
+{
     public function index()
-    {   
-        $product = new Product();
+    {
+        // Check if the user is logged in
+        if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_email']) || !isset($_SESSION['user_type'])) {
+            // User is not logged in, redirect to the login page
+            header('Location: ' . ROOT . '/login');
+            exit();
+        }
 
-      
-        $products = $product->get_latest_products();
-        
-        $this->view('home', ['products' => $products]);
-    }   
+        // Fetch user details based on the user type
+        // if ($_SESSION['user_type'] === 'CUSTOMER') {
+        //     $customerModel = new Customer();
+        //     $user = $customerModel->getById($_SESSION['user_id']);
+        // } elseif ($_SESSION['user_type'] === 'ADMIN') {
+        //     $adminModel = new Admin();
+        //     $user = $adminModel->getById($_SESSION['user_id']);
+        // }
+
+        // Pass user data to the view
+        // $this->view('account', ['user' => $user]);
+        $this->view('home');
+    }
 }
 
-
-
-
+?>
