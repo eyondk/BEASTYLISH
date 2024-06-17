924f9b10bdddf805ad94467487b2196b41f88f5d
Christine Jade L. Duran <jadeduranchristine@gmail.com>
2024-06-18 01:39:13 +0800

    Update Model.php

--- app/core/Model.php
+++ app/core/Model.php
@@ -36,56 +36,77 @@
         
     //     return $this->query($query, $data);
     // }
+
+
+
+    // public function where($data, $data_not = [])
+    // {
+    // $keys = array_keys($data);
+    // $keys_not = array_keys($data_not);
+    // $query = "select * from $this->table where ";
+
+    // foreach ($keys as $key) {
+    // $query .= $key . " = :". $key . " && ";
+    // }
+
+    // foreach ($keys_not as $key) {
+    // $query .= $key . " != :". $key . " && ";
+    // }
     
+    // $query = trim($query," && ");
 
+    // $query .= " order by $this->order_column $this->order_type limit $this->limit offset $this->offset";
+    // $data = array_merge($data, $data_not);
 
-    public function where($data, $data_not = [])
-    {
+    // return $this->query($query, $data);
+    // }
+
+    public function where($conditions) {
+        $query = "SELECT * FROM {$this->table} WHERE ";
+        $values = [];
+        foreach ($conditions as $column => $value) {
+            $query .= "{$column} = ? AND ";
+            $values[] = $value;
+        }
+        $query = rtrim($query, ' AND ');
+        $query .= " ORDER BY add_id DESC"; // Use add_id if ordering is needed
+        $query .= " LIMIT 1"; // If you want to limit the results to 1
+
+        return $this->query($query, $values);
+    }
+
+    public function first($data, $data_not = []) {
     $keys = array_keys($data);
     $keys_not = array_keys($data_not);
     $query = "select * from $this->table where ";
 
     foreach ($keys as $key) {
-    $query .= $key . " = :". $key . " && ";
+        $query .= $key . " = :". $key . " && ";
     }
 
     foreach ($keys_not as $key) {
-    $query .= $key . " != :". $key . " && ";
+        $query .= $key . " != :". $key . " && ";
     }
-    
+
     $query = trim($query," && ");
 
-    $query .= " order by $this->order_column $this->order_type limit $this->limit offset $this->offset";
-    $data = array_merge($data, $data_not);
-
-    return $this->query($query, $data);
-    }
-
-    public function first($data, $data_not = [])
-    {
-    $keys = array_keys($data);
-    $keys_not = array_keys($data_not);
-    $query = "select * from $this->table where ";
-
-    foreach ($keys as $key) {
-    $query .= $key . " = :". $key . " && ";
-    }
-
-    foreach ($keys_not as $key) {
-    $query .= $key . " != :". $key . " && ";
-    }
-    
-    $query = trim($query," && ");
-
-    $query .= " limit $this->limit offset $this->offset";
+    $query .= " limit 1"; // Adjusted limit to 1 for single record
     $data = array_merge($data, $data_not);
     
+    // Debugging output
+    // echo "Constructed query: " . $query . "<br>";
+    // echo "Query parameters: <pre>" . print_r($data, true) . "</pre>";
+
     $result = $this->query($query, $data);
     if($result)
-    return $result[0];
+        return $result[0];
 
     return false;
     }
+
+
+   
+
 
     //modified
     public function insert($data)
@@ -147,6 +168,48 @@
     return false;
 
     }
+
+    public function getLastInsertedCustomerID() {
+        $query = "SELECT currval('customer_cus_id_seq') AS last_insert_id";
+        $result = $this->query($query);
+        if ($result && isset($result[0]['last_insert_id'])) {
+            return $result[0]['last_insert_id'];
+        }
+        return false;
+    }
+    
+    
+
+    public function firsttoken($data, $data_not = []) {
+        $keys = array_keys($data);
+        $keys_not = array_keys($data_not);
+        $query = "SELECT * FROM $this->table WHERE ";
+    
+        foreach ($keys as $key) {
+            $query .= $key . " = :" . $key . " AND ";
+        }
+    
+        foreach ($keys_not as $key) {
+            $query .= $key . " != :" . $key . " AND ";
+        }
+    
+        $query = rtrim($query, " AND ");
+        $query .= " LIMIT $this->limit OFFSET $this->offset";
+    
+        $data = array_merge($data, $data_not);
+    
+        // Debugging output
+        echo "Constructed query: " . $query . "<br>";
+        echo "Query parameters: <pre>" . print_r($data, true) . "</pre>";
+    
+        $result = $this->query($query, $data);
+        if ($result) {
+            return $result[0];
+        }
+    
+        return false;
+    }
+    
 
 }
  
